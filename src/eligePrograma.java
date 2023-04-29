fe79475dcb6457e8b560441e885908ee01b45d2c
Carlos Mendoza <Carlos.mendoza@gmail.com>
2023-04-29 14:55:42 -0600

    validar usurio con password y usuario listo

--- src/eligePrograma.java
+++ src/eligePrograma.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class eligePrograma {
      public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          String[] usuario = new String[2];
+          usuario[0] = "jazmin";
+          usuario[1] = "carlos";
+
+          String[] password = new String[2];
+          password[0] = "1325423";
+          password[1] = "1425581";
+
+          System.out.print("Ingresa tu usuario: ");
+          String u = scanner.nextLine();
+          System.out.print("Ingresa tu contraseña");
+          String p = scanner.nextLine();
+
+          boolean valida = false;
+          for (int i=0; i< usuario.length; i++){
+               if (usuario[i].equalsIgnoreCase(u) && password[i].equalsIgnoreCase(p)){
+                    valida = true;
+               }
+          }
+
+          if (valida){
+               System.out.println("Bienvenid@");
+          }else {
+               System.out.println("Requiere validar");
+          }
+
+
+
 
      }
 }
